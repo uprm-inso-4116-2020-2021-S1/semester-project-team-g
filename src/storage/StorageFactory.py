7c5dcdc7d873f1610f7745964f305915fb6ecf41
markbookk <marcos.nieves1@upr.edu>
2020-09-03 16:30:49 -0400

    Added handling of high-order functions

--- src/storage/StorageFactory.py
+++ src/storage/StorageFactory.py
@@ -9,12 +9,18 @@ from FileStorageStrategy import FileStorageStrategy
 
 class StorageFactory:
     def __init__(self, fileStorageStrategy: FileStorageStrategy):
+        self.strategy = self.generateStrategy()
         self.fileStorageStrategy = fileStorageStrategy
     
     def generateStrategy(self):
+        if self.strategy:
+            return self.strategy
         inDevelopment = os.getenv("IN_DEVELOPMENT")
         if inDevelopment:
             return self.fileStorageStrategy()
         else:
             # Here we would be passing a cloud or similar StorageStrategy implementation
             pass
+    
+    def getFunctions(self):
+        return self.strategy.formatData, self.strategy.postData, self.strategy.getData, self.strategy.getPersonData
--- src/storage/StorageService.py
+++ src/storage/StorageService.py
@@ -5,16 +5,23 @@ from StorageFactory import StorageFactory
 
 class StorageService:
     def __init__(self, storageFactory: StorageFactory):
-        self.StorageStrategy = storageFactory.createStrategy()
+        self.storageStrategy = storageFactory.createStrategy()
+
+        # Handle high-order functions
+        self._formatData, self._postData, self._getData, self._getPersonData = storageFactory.getFunctions()
         
     def formatData(self, data: str):
+        self._formatData(data)
         pass
 
     def postData(self, data: dict): 
+        self._postData(data)
         pass
 
     def getData(self, dataType: int, parameters: dict = {}):
+        self._getData(dataType, parameters)
         pass
 
     def getPersonData(self, SSN: int): 
+        self._getPersonData(SSN)
         pass
