ba6fb733b2277a8ea6a9a34bf364b2c6facba4cb
Christian Alapont <christianalapont@Miller-the-MacBook-Pro.local>
2020-09-14 15:33:31 -0400

    Added some possible functions to handler and DAO.

--- API/Handler/CitizenHandler.py
+++ API/Handler/CitizenHandler.py
@@ -1,3 +1,15 @@
 """ First DB must be built"""
 class CitizenHandler:
-    pass
+    
+    def get_global_results():
+        pass
+
+    def get_results_by_municapility(municipality):
+        pass
+
+    def get_results_by_sex(sex):
+        pass
+
+    def get_results_by_age(min_age, max_age):
+        pass
+
