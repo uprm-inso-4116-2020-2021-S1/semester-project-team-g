2005f8cd77f89341fb51b8632c5fd3ad72bc203a
Jose Biescas <jose.biescas@upr.edu>
2020-10-03 16:47:03 -0400

    Added routes for CitizenHandler. Implemented dummy CitizenHandler. Moved directories around.

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 import os
 from gevent.pywsgi import WSGIServer
-from routes import cth_routes
+from API.routes import cth_routes
 
 #App instance
 app = Flask("CTH")
 
 #Routes
-app.add_url_rule('/', view_func=cth_routes.dummy, methods=['GET'])
+app.add_url_rule('/results-global', view_func=cth_routes.get_global_results, methods=['GET'])
+
+app.add_url_rule('/results-age/<min_age>&<max_age>/<illness>', view_func=cth_routes.get_results_by_age, methods=['GET'])
+app.add_url_rule('/results-age/<min_age>&<max_age>', view_func=cth_routes.get_results_by_age, methods=['GET'], defaults={'illness': None})
+
+app.add_url_rule('/results-municipality/<municipality>/<illness>', view_func=cth_routes.get_results_by_municipality, methods=['GET'])
+app.add_url_rule('/results-municipality/<municipality>', view_func=cth_routes.get_results_by_municipality, methods=['GET'], defaults={'illness': None})
+
+app.add_url_rule('/results-sex/<sex>/<illness>', view_func=cth_routes.get_results_by_sex, methods=['GET'])
+app.add_url_rule('/results-sex/<sex>', view_func=cth_routes.get_results_by_sex, methods=['GET'], defaults={'illness': None})
+
 
 #Run
 if __name__ == '__main__':
--- routes/cth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import request, make_response, render_template
-import requests
-'''
-Place routes here as basic python function.
-'''
-def dummy():
-    request.form['payload']
