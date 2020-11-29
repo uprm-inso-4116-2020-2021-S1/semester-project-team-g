80387325bc39b84473a54ab1839d2c808c3b9f8e
Jose Biescas <jose.biescas@upr.edu>
2020-11-29 11:41:15 -0400

    Statistics displays results of searches. Displays some errors if any. Modified css. Modified statistics actions to use async/await.

--- client/src/actions/filterActions.js
+++ client/src/actions/filterActions.js
@@ -1,54 +1,128 @@
 import axios from "axios";
 import { GET_ERRORS } from "./types";
 
-export const globalFilter = () => async dispatch => {
-  let response = await axios.get("http://localhost:5000/results-global");
+// export const globalFilter = (filterData) => async dispatch => {
+//   let response = await axios.get("http://localhost:5000/results-global");
+//   let data = await response.data;
+//   return data;
+// };
+
+export const globalFilter = (filterData) => async (dispatch) => {
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let response = await axios
+    .get(`http://localhost:5000/${pType}/results-global`)
+    .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
   let data = await response.data;
-  return data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+    return data;
+  } else {
+    return {};
+  }
 };
 
 export const ageFilter = (filterData) => async (dispatch) => {
-    let response = await axios
-        .get(`http://localhost:5000/results-age/${filterData.min_age}&${filterData.max_age}`)
-        .catch((err) =>
-            dispatch({
-                type: GET_ERRORS,
-                payload: err.response.data
-            })
-        )
-    let data = await response.data;
-    if (data) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        })
-        return data;
-    }
-    else {
-        return {}
-    }
-}
-
-  export const municipalityFilter = (filterData) => async dispatch => {
-    let response = await axios.get(`http://localhost:5000/results-municipality/${filterData.municipality}`);
-    let data = await response.data;
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let response = await axios
+    .get(
+      `http://localhost:5000/${pType}/results-age/${filterData.min_age}&${filterData.max_age}`
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+  let data = await response.data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
     return data;
-  };
+  } else {
+    return {};
+  }
+};
 
-  export const sexFilter = (filterData) => async dispatch => {
-    let response = await axios.get(`http://localhost:5000/results-sex/${filterData.sex}`);
-    let data = await response.data;
+export const municipalityFilter = (filterData) => async (dispatch) => {
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let illness = filterData.illness ? `/${filterData.illness}` : "";
+  let response = await axios
+    .get(
+      `http://localhost:5000/${pType}/results-municipality/${filterData.municipality}${illness}`
+    )
+    .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+  let data = await response.data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
     return data;
-  };
+  } else {
+    return {};
+  }
+};
 
-  export const monthFilter = (filterData) => async dispatch => {
-    let response = await axios.get(`http://localhost:5000/results-month/${filterData.month}`);
-    let data = await response.data;
+export const sexFilter = (filterData) => async (dispatch) => {
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let illness = filterData.illness ? `/${filterData.illness}` : "";
+  let response = await axios
+    .get(
+      `http://localhost:5000/${pType}/results-sex/${filterData.sex}${illness}`
+    )
+    .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+  let data = await response.data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
     return data;
-  };
+  } else {
+    return {};
+  }
+};
 
-  export const yearFilter = (filterData) => async dispatch => {
-    let response = await axios.get(`http://localhost:5000/results-year/${filterData.year}`);
-    let data = await response.data;
+export const monthFilter = (filterData) => async (dispatch) => {
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let illness = filterData.illness ? `/${filterData.illness}` : "";
+  let response = await axios
+    .get(
+      `http://localhost:5000/${pType}/results-month/${filterData.month}${illness}`
+    )
+    .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+  let data = await response.data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
     return data;
-  };
+  } else {
+    return {};
+  }
+};
+
+export const yearFilter = (filterData) => async (dispatch) => {
+  let pType = filterData.patientType === "infected" ? "infected" : "recovered";
+  let illness = filterData.illness ? `/${filterData.illness}` : "";
+  let response = await axios.get(
+    `http://localhost:5000/${pType}/results-year/${filterData.year}${illness}`
+  )
+  .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+  let data = await response.data;
+  if (data) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+    return data;
+  } else {
+    return {}
+  }
+};
